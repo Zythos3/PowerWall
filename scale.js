058fb765315a96880957fb9b70c29d09f1daf669
finkegabriel <40010444+finkegabriel@users.noreply.github.com>
2018-06-16 00:38:08 -0700

    Delete scale.js

--- scale.js
+++
@@ -1,4 +0,0 @@
-var scale = 'scale(1)';
-document.body.style.webkitTransform =  scale;    // Chrome, Opera, Safari
-document.body.style.msTransform =   scale;       // IE 9
-document.body.style.transform = scale;     // General//work
