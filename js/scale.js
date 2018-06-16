cd97f8f24c4be04cecb7eb644f99eebc9b1c3449
Gabriel Finke <finkegabriel@gmail.com>
2018-06-16 00:36:23 -0700

    organize

---
+++ js/scale.js
@@ -0,0 +1,4 @@
+var scale = 'scale(1)';
+document.body.style.webkitTransform =  scale;    // Chrome, Opera, Safari
+document.body.style.msTransform =   scale;       // IE 9
+document.body.style.transform = scale;     // General//work
